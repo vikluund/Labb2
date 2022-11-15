89c209e282f9138fd0d7f9c30430e4a55d4f7988
Tim <112405922+vikluund@users.noreply.github.com>
2022-11-15 16:01:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,18 +13,24 @@
 // Get everything from DOM
 // Get Input
 
-let result = document.getElementById('result');
+let result = document.getElementById('results');
 
 // Submit button
 
-let button = document.querySelector('#submitButton');
+let button = document.querySelector('#btn');
+
+
 
 
 
 function country() {
-  let url = 'https://date.nager.at/api/v3/publicholidays/2022/'+country;
-  let getCountry = document.querySelector('#country');
-  let country = getCountry.value;
+
+  let selectCountry = document.querySelector('#country');
+  let country = selectCountry.value;
+
+  let url = 'https://date.nager.at/api/v3/publicholidays/2022/'+ country;
+
+
 
   fetch(url)
   .then(response => response.json())
@@ -53,3 +59,56 @@ function country() {
     })
   })
 }
+
+button.addEventListener('click', event => {
+  event.preventDefault();
+  country();
+});
+
+let selectCountry = document.querySelector('#country');
+selectCountry.addEventListener('input', () => {
+
+  result.innerHTML = ``
+});
+
+
+fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+.then((response) => response.json())
+.then((result) => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const data = [],
+  labels = [];
+
+  for(let n = 0; n < result.length; n++) {
+    const holiday = result[n]
+
+    data.push(holiday.Population)
+    labels.push(holiday.Year)
+  }
+  console.log('data', data);
+  console.log('labels', labels);
+})
+
+
+  // CHART JS
+
+  // const ctx = document.getElementById('myChart');
+
+  // new Chart(ctx, {
+  //   type: 'bar',
+  //   data: {
+  //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //     datasets: [{
+  //       label: '# of Votes',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       borderWidth: 1
+  //     }]
+  //   },
+  //   options: {
+  //     scales: {
+  //       y: {
+  //         beginAtZero: true
+  //       }
+  //     }
+  //   }
+  // });
